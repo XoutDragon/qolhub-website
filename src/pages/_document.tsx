2dbff14452f3e9f3ff122fdb71b3149bbe29d748
Jason Wang <83075516+XoutDragon@users.noreply.github.com>
2022-10-29 22:36:38 -0400

    Fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html style={{ scrollBehavior: 'smooth' }}>
-      <Head />
+      <Head>
+        {' '}
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-T0Z84T04VW', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
